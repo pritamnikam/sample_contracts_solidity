5b59610073a72c5c111b1dbd656f7dcb8a235429
Pritam Nikam <pritam.nikam@gmail.com>
2022-06-18 06:05:01 -0600

    ERC 721 Token

---
+++ MyNFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+// This contract will inherit from the Ownable and NFT Token Metadata contracts from 0xcert 
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+
+contract MyNFT is NFTokenMetadata, Ownable {
+
+    // Contract constructor.
+    //   Runs at deployment.
+    //   Sets name and symbol of NFT.
+    constructor() {
+        nftName = "MyNFT";
+        nftSymbol = "MNF";
+    } // constructor
+
+    // This function will mint a new NFT and deposit it in the wallet at the _to address.
+    //   Each token must have a Token ID.
+    //   The URL property indicates the digital asset the token holder has access to.
+    function mint(address _to, uint256 _tokenId, string calldata _url) external onlyOwner {
+        super._mint(_to, _tokenId);
+        super._setTokenUri(_tokenId, _url);
+    } // mint
+
+} // MyNFT
